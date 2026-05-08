62aa8d39fbc5e4768ad1fb7426f449a2596941df
sivarajapratheep175 <sivarajapratheep175@gmail.com>
2026-05-08 16:16:58 +0530

    Add user name prompt and confirmation in script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+let input = prompt("Enter your name? (I want to know who is viewing my portfolio) :", "Full Name");
+let checking = confirm("Your name is " + input + ". Is that correct?");
+ console.log(input);
+ console.log(checking);  
